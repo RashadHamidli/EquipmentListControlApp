382e4b8f4fe0b66319a5d2f3143e3f6a19e4dc9c
RashadHamidli <mr_rashad@email.com>
2024-02-18 22:25:39 +0400

    add

--- src/main/java/com/katv1/controller/EquipmentFindController.java
+++ src/main/java/com/katv1/controller/EquipmentFindController.java
@@ -4,9 +4,7 @@ import com.katv1.entity.Equipment;
 import com.katv1.service.EquipmentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +18,11 @@ public class EquipmentFindController {
     }
 
     @GetMapping("/find")
-    public String getOneEquipmentByAddress(@RequestParam("st") String street,
-                                           @RequestParam("bld") String build, Model model) {
-        List<Equipment> list = equipmentService.findOneEquipmentByAddress(street, build);
+    public String getOneEquipmentByAddress(@RequestParam("street") String street,
+                                           @RequestParam("build") String build, Model model) {
+        String streetTrim = street.trim();
+        String buildTrim = build.trim();
+        List<Equipment> list = equipmentService.findOneEquipmentByAddress(streetTrim, buildTrim);
         model.addAttribute("equipmentList", list);
         return "equipment_find";
     }
